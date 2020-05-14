5a269c89e02f98ff57f6abf130cfdc86745475ca
Adarsh Jha <adarshjha51@gmail.com>
2020-05-14 02:48:48 -0700

    Added track progress bar and quiz stats

--- QuizApp/app/src/main/java/com/example/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.java
@@ -6,22 +6,32 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView quizStatsTextView;
+    private ProgressBar quizPB;
     Button trueBtn, wrongBtn;
     TextView questionTextView;
     private int questionIndex;
     private  int questionQuiz;
+    private int score;
 
 //    This array is create for only use to structure and packaged data because we are not using database
 
     private QuizModel[] quizQuestions = new QuizModel[]{
 
-            new QuizModel(R.string.q1,true), new QuizModel(R.string.q2, false), new QuizModel(R.string.q3, true)
+            new QuizModel(R.string.q1,false), new QuizModel(R.string.q2, false),
+            new QuizModel(R.string.q3, true),
+            new QuizModel(R.string.q4,false), new QuizModel(R.string.q5,false), new QuizModel(R.string.q6, true),
+            new QuizModel(R.string.q7,true), new QuizModel(R.string.q8,false), new QuizModel(R.string.q9,true),
+            new QuizModel(R.string.q10, true)
     };
+
+    final int USER_PROGRESS = (int) Math.ceil(100.0 / quizQuestions.length);
 
 
 
@@ -32,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         trueBtn = findViewById(R.id.trueBtn);
         wrongBtn = findViewById(R.id.wrongBtn);
         questionTextView = findViewById(R.id.questionTextView);
+        quizStatsTextView = findViewById(R.id.quizStatsTextView);
+        quizPB = findViewById(R.id.quizPB);
 
 //        Object created to access the data
         QuizModel q1 = quizQuestions[0];
@@ -62,9 +74,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateQuestion(){
 
+        quizStatsTextView.setText(String.valueOf(score));
         questionIndex = (questionIndex + 1) % quizQuestions.length;
         questionQuiz = quizQuestions[questionIndex].getmQuestion();
         questionTextView.setText(questionQuiz);
+        quizPB.incrementProgressBy(USER_PROGRESS);
 
     }
 
@@ -75,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (correctAnswer == userGuess){
 
+            score += 1;
             Toast.makeText(getApplicationContext(),R.string.correct_answer_toast,Toast.LENGTH_SHORT).show();
         }
         else{
